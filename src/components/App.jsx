173a38d685215da58831a5a62450439d0feb666c
Imamova Maria <maria.g4vr1lova@yandex.ru>
2023-07-19 20:50:56 +0300

    Mesto-react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,191 +1,201 @@
-import Header from './Header/Header.jsx'
-import Main from './Main/Main.jsx';
-import Footer from './Footer/Footer.jsx';
-import PopupWithForm from './PopupWithForm/PopupWithForm.jsx';
-import ImagePopup from './ImagePopup/ImagePopup.jsx';
-import { useCallback, useEffect, useState } from 'react';
-import CurrentUserContext from '../contexts/CurrentUserContext.js'
-import api from '../utils/api.js';
-import EditProfilePopup from './EditProfilePopup/EditProfilePopup.jsx';
-import EditAvatarPopup from './EditAvatarPopup/EditAvatarPopup.jsx';
-import AddPlacePopup from './AddPlacePopup/AddPlacePopup.jsx';
+import Header from "./Header/Header.jsx";
+import Main from "./Main/Main.jsx";
+import Footer from "./Footer/Footer.jsx";
+import PopupWithForm from "./PopupWithForm/PopupWithForm.jsx";
+import ImagePopup from "./ImagePopup/ImagePopup.jsx";
+import { useCallback, useEffect, useState } from "react";
+import CurrentUserContext from "../contexts/CurrentUserContext.js";
+import api from "../utils/api.js";
+import EditProfilePopup from "./EditProfilePopup/EditProfilePopup.jsx";
+import EditAvatarPopup from "./EditAvatarPopup/EditAvatarPopup.jsx";
+import AddPlacePopup from "./AddPlacePopup/AddPlacePopup.jsx";
 
 function App() {
   //стейты попапа
-  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false)
-  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false)
-  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false)
-  const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false)
-  const [selectedCard, setSelectedCard ] = useState({})
-  const [isImagePopup, setIsImagePopup] = useState(false)
-  const [isSend, setIsSend] = useState(false)
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
+  const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState({});
+  const [isImagePopup, setIsImagePopup] = useState(false);
+  const [isSend, setIsSend] = useState(false);
   //стейты контекста
-  const [currentUser, setCurrentUser] = useState({})
+  const [currentUser, setCurrentUser] = useState({});
   //стейты карточки
-  const [cards, setCards] = useState ([])
-  const [isLoadingCards, setIsLoadingCards] = useState(true)
-  const [deleteCardId, setDeleteCardId] = useState('')
+  const [cards, setCards] = useState([]);
+  const [isLoadingCards, setIsLoadingCards] = useState(true);
+  const [deleteCardId, setDeleteCardId] = useState("");
 
-
-  const setAllStatesForClosePopups = useCallback ( () => {
-    setIsEditProfilePopupOpen(false)
-    setIsAddPlacePopupOpen(false)
-    setIsEditAvatarPopupOpen(false)
-    setIsDeletePopupOpen(false)
-    setIsImagePopup(false)
-  }, [])
-  const closePopupByEsc = useCallback((evt) => {
-    if (evt.key === 'Escape') {
-      setAllStatesForClosePopups()
-      document.removeEventListener('keydown', closePopupByEsc)
-    }
-  },[setAllStatesForClosePopups])
+  const setAllStatesForClosePopups = useCallback(() => {
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setIsEditAvatarPopupOpen(false);
+    setIsDeletePopupOpen(false);
+    setIsImagePopup(false);
+  }, []);
+  const closePopupByEsc = useCallback(
+    (evt) => {
+      if (evt.key === "Escape") {
+        closeAllPopups();
+      }
+    },
+    [setAllStatesForClosePopups]
+  );
   const closeAllPopups = useCallback(() => {
-    setAllStatesForClosePopups()
-    document.removeEventListener('keydown', closePopupByEsc)
-  }, [setAllStatesForClosePopups, closePopupByEsc])
+    setAllStatesForClosePopups();
+    document.removeEventListener("keydown", closePopupByEsc);
+  }, [setAllStatesForClosePopups, closePopupByEsc]);
   function setEvantListnerForDocument() {
-    document.addEventListener('keydown', closePopupByEsc)
+    document.addEventListener("keydown", closePopupByEsc);
   }
   function handleEditProfileClick() {
-    setIsEditProfilePopupOpen(true)
-    setEvantListnerForDocument()
+    setIsEditProfilePopupOpen(true);
+    setEvantListnerForDocument();
   }
   function handleEditAvatarClick() {
-    setIsEditAvatarPopupOpen(true)
-    setEvantListnerForDocument()
-
+    setIsEditAvatarPopupOpen(true);
+    setEvantListnerForDocument();
   }
   function handleAddPlaceClick() {
-    setIsAddPlacePopupOpen(true)
-    setEvantListnerForDocument()
+    setIsAddPlacePopupOpen(true);
+    setEvantListnerForDocument();
   }
   function handleDeletePopupClick(cardId) {
-    setDeleteCardId(cardId)
-    setIsDeletePopupOpen(true)
-    setEvantListnerForDocument()
+    setDeleteCardId(cardId);
+    setIsDeletePopupOpen(true);
+    setEvantListnerForDocument();
   }
   function handleCardClick(card) {
-    setSelectedCard(card)
-    setIsImagePopup(true)
-    setEvantListnerForDocument()
+    setSelectedCard(card);
+    setIsImagePopup(true);
+    setEvantListnerForDocument();
   }
   useEffect(() => {
-    setIsLoadingCards(true)
+    setIsLoadingCards(true);
     Promise.all([api.getInfo(), api.getCards()])
-        .then(([dataUser, dataCard]) => {
-            setCurrentUser(dataUser)  
-            setCards(dataCard)
-            setIsLoadingCards(false)
-        })
-    .catch((error => console.error('Ошибка при получении данных с сервера о пользователе и карточках' `${error}`)))
-  }, [])
+      .then(([dataUser, dataCard]) => {
+        setCurrentUser(dataUser);
+        setCards(dataCard);
+        setIsLoadingCards(false);
+      })
+      .catch((error) =>
+        console.error(
+          "Ошибка при получении данных с сервера о пользователе и карточках"`${error}`
+        )
+      );
+  }, []);
   function handleCardDelete(evt) {
-    evt.preventDefault()
-    setIsSend(true)
-    api.deleteCard(deleteCardId)
-    .then(() => {
-      setCards(cards.filter(card => {
-        return card._id !== deleteCardId
-      }))
-      closeAllPopups()
-      setIsSend(false)
-    })
-    .catch((error => console.error('Ошибка при удалении карточки' `${error}`)))
-    .finally(() => setIsSend(false))
+    evt.preventDefault();
+    setIsSend(true);
+    api
+      .deleteCard(deleteCardId)
+      .then(() => {
+        setCards(
+          cards.filter((card) => {
+            return card._id !== deleteCardId;
+          })
+        );
+        closeAllPopups();
+      })
+      .catch((error) => console.error("Ошибка при удалении карточки"`${error}`))
+      .finally(() => setIsSend(false));
   }
   function handleUpdateUser(dataUser, reset) {
-    setIsSend(true)
-    api.setUserInfo(dataUser)
-      .then(res =>{
-        setCurrentUser(res)
-        closeAllPopups()
-        reset()
-        setIsSend(false)
+    setIsSend(true);
+    api
+      .setUserInfo(dataUser)
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+        reset();
       })
-      .catch((error => console.error('Ошибка при редактировании профиля' `${error}`)))
-      .finally(() => setIsSend(false))
+      .catch((error) =>
+        console.error("Ошибка при редактировании профиля"`${error}`)
+      )
+      .finally(() => setIsSend(false));
   }
   function handleUpdateAvatar(dataUser, reset) {
-    setIsSend(true)
-    api.setNewAvatar(dataUser)
-    .then(res => {
-      setCurrentUser(res)
-      closeAllPopups()
-      reset()
-      setIsSend(false)
-    })
-    .catch((error => console.error('Ошибка при редактировании ававтара' `${error}`)))
-    .finally(() => setIsSend(false))
-  }
-  function handleAddPlaceSubmit (dataCard, reset) {
-    setIsSend(true)
-    api.addCard(dataCard)
-      .then(res => {
-        setCards([res, ...cards])
-        closeAllPopups()
-        reset()
-        setIsSend(false)
+    setIsSend(true);
+    api
+      .setNewAvatar(dataUser)
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+        reset();
       })
-      .catch((error => console.error('Ошибка при добавлении карточки' `${error}`)))
-      .finally(() => setIsSend(false))
+      .catch((error) =>
+        console.error("Ошибка при редактировании ававтара"`${error}`)
+      )
+      .finally(() => setIsSend(false));
+  }
+  function handleAddPlaceSubmit(dataCard, reset) {
+    setIsSend(true);
+    api
+      .addCard(dataCard)
+      .then((res) => {
+        setCards([res, ...cards]);
+        closeAllPopups();
+        reset();
+      })
+      .catch((error) =>
+        console.error("Ошибка при добавлении карточки"`${error}`)
+      )
+      .finally(() => setIsSend(false));
   }
   return (
-  <CurrentUserContext.Provider value={currentUser}>
-    <div className="page__content">
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page__content">
+        <Header />
 
-    <Header />
+        <Main
+          onEditProfile={handleEditProfileClick}
+          onAddPlace={handleAddPlaceClick}
+          onEditAvatar={handleEditAvatarClick}
+          onCardClick={handleCardClick}
+          onDelete={handleDeletePopupClick}
+          cards={cards}
+          isLoading={isLoadingCards}
+        />
 
-    <Main
-    onEditProfile = {handleEditProfileClick}
-    onAddPlace = {handleAddPlaceClick}
-    onEditAvatar = {handleEditAvatarClick}
-    onCardClick = {handleCardClick}
-    onDelete = {handleDeletePopupClick}
-    cards = {cards}
-    isLoading = {isLoadingCards}
-    />
+        <Footer />
 
-    <Footer />
+        <EditProfilePopup
+          onUpdateUser={handleUpdateUser}
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          isSend={isSend}
+        />
 
-    <EditProfilePopup
-      onUpdateUser = {handleUpdateUser}
-      isOpen = {isEditProfilePopupOpen}
-      onClose = {closeAllPopups}
-      isSend = {isSend}
-    />
+        <AddPlacePopup
+          onAddPlace={handleAddPlaceSubmit}
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          isSend={isSend}
+        />
 
-    <AddPlacePopup
-      onAddPlace = {handleAddPlaceSubmit}
-      isOpen = {isAddPlacePopupOpen}
-      onClose = {closeAllPopups}
-      isSend = {isSend}
-    />
+        <EditAvatarPopup
+          onUpdateAvatar={handleUpdateAvatar}
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          isSend={isSend}
+        />
 
-    <EditAvatarPopup
-      onUpdateAvatar = {handleUpdateAvatar}
-      isOpen = {isEditAvatarPopupOpen}
-      onClose = {closeAllPopups}
-      isSend = {isSend}
-    />
-
-    <PopupWithForm 
-      name='popup-delete'
-      title = 'Вы уверены?'
-      titleButton = 'Да'
-      isOpen={isDeletePopupOpen}
-      onClose = {closeAllPopups}
-      onSubmit = {handleCardDelete}
-      isSend = {isSend}
-    /> 
-    <ImagePopup
-      card = {selectedCard}
-      isOpen = {isImagePopup}
-      onClose = {closeAllPopups}
-    /> 
-      
-  </div>
-  </CurrentUserContext.Provider>
+        <PopupWithForm
+          name="popup-delete"
+          title="Вы уверены?"
+          titleButton="Да"
+          isOpen={isDeletePopupOpen}
+          onClose={closeAllPopups}
+          onSubmit={handleCardDelete}
+          isSend={isSend}
+        />
+        <ImagePopup
+          card={selectedCard}
+          isOpen={isImagePopup}
+          onClose={closeAllPopups}
+        />
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,9 @@
-import logo from '../../images/logo.svg'
+import logo from "../../images/logo.svg";
 function Header() {
-    return(
-        <header className="header">
-      <img
-        className="header__logo"
-        src={logo}
-        alt="Место"
-      />
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="Место" />
     </header>
-    ) 
+  );
 }
-export default Header
+export default Header;
